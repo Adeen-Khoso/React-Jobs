33e38505fef4beb6419a7300bd05f8bbb5c70a74
Adeen <adeenkhoso@gmail.com>
2024-09-23 17:29:28 +0500

    JobsPage updated

--- src/Pages/JobsPage.jsx
+++ src/Pages/JobsPage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import JobListings from "../components/JobListings";
 
 const JobsPage = () => {
-  return <div>JobsPage</div>;
+  return <section className="px-4 py-2 bg-blue-50">
+    <JobListings />
+  </section>
 };
 
 export default JobsPage;
